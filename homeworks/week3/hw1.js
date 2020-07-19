6fc07ae8864f798ec41fdc4dd73951a59220e39b
Newman Jian <newmanjian.mba04g@g2.nctu.edu.tw>
2020-07-19 17:14:37 +0800

    week3

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,15 +1,29 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+const lines = [];
+
+rl.on('line', (line) => {
+  lines.push(line);
+});
+
 function printStar(n) {
   let res = '';
-  for (let i = 1; i <= n && n <= 30; i += 1) {
+  for (let i = 1; i <= n; i += 1) {
     res += '*';
   }
   console.log(res);
 }
 
-function stars(n) {
-  for (let i = 1; i <= n; i += 1) {
+function solve(line) {
+  for (let i = 1; i <= line; i += 1) {
     printStar(i);
   }
 }
 
-stars(10);
+rl.on('close', () => {
+  solve(lines);
+});
